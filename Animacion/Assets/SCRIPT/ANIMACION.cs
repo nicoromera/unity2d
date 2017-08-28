603df12cf90920642a47caae55fa5d08351881dc
NICUS-PC\Nicus <nicusis@gmail.com>
2017-08-28 17:56:33 -0300

    animacion sonic

---
+++ Animacion/Assets/SCRIPT/ANIMACION.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ANIMACION : MonoBehaviour {
+    
+    Animator anim;
+	
+	void Awake () {
+        anim = GetComponent<Animator>();
+	}
+	
+	
+	void Update () {
+        //caminar
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            anim.SetBool("isWalking", true);
+        if (Input.GetKeyUp(KeyCode.Alpha1))
+            anim.SetBool("isWalking", false);
+        //correr
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            anim.SetBool("IsRunning", true);
+        if (Input.GetKeyUp(KeyCode.Alpha2))
+            anim.SetBool("IsRunning", false);
+        //saltar
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            anim.SetBool("isJumping", true);
+        if (Input.GetKeyUp(KeyCode.Alpha3))
+            anim.SetBool("isJumping", false);
+        //bola
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+            anim.SetBool("isBolita", true);
+        if (Input.GetKeyUp(KeyCode.Alpha4))
+            anim.SetBool("isBolita", false);
+    }
+}
